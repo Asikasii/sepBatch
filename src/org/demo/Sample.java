42e37d0aba5d9b82d3fe1c8ec0f23635166dac73
Asikasii <mohamedasik030@gmail.com>
2025-09-25 11:28:08 +0530

    done

--- src/org/demo/Sample.java
+++ src/org/demo/Sample.java
@@ -5,4 +5,16 @@ public class Sample {
 	private void empName() {
 System.out.println("Employee Name : Suresh");
 	}
+	
+	private void empNam() {
+  System.out.println("Employee Name : Asik");
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		Sample s = new Sample();
+		s.empNam();
+		s.empName();
+	}
 }
